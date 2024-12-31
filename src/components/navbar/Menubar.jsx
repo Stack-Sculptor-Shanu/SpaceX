f35f8e9c1a3c8a1d07d8e36420fe16a29675860e
Stack-Sculptor-Shanu <amarjeetjtpr@gmail.com>
2024-12-31 15:38:59 +0530

    SpaceX

---
+++ src/components/navbar/Menubar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RiMenuLine } from "react-icons/ri";
+const Menubar = () => {
+  return (
+    <div className='menu'>
+        <span>shop</span>
+        <span><RiMenuLine /></span>
+    </div>
+  )
+}
+
+export default Menubar
